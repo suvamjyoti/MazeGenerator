fde2f8f99d517854de208e6a91fa9c032e687828
Suvamjyoti_Panda <likun54@gmail.com>
2021-01-10 12:56:25 +0530

    A Basic Maze rendered(no path)

---
+++ Assets/Scripts/MazeCreator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//A Maze is a collection of node.
+//connect certain node to create a path.
+//place walls at unconnected edges.
+//and we get a maze.
+
+
+public enum StateOfWall{                        // State of each wall of a node.
+    Left,Right,Up,Down,        
+}
+
+
+public static class MazeCreator {
+
+    public static StateOfWall[,] Create(int width,int height){
+
+        StateOfWall[,] maze = new StateOfWall[width,height];
+
+        StateOfWall initial = StateOfWall.Left | StateOfWall.Right | StateOfWall.Up | StateOfWall.Down;
+    
+        for(int i=0;i<width;i++){
+            for(int j=0;j<height;j++){
+                maze[i,j] = initial;
+            }
+        }
+
+        return maze;
+    }
+}
---
+++ Assets/Scripts/MazeRenderer.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeRenderer : MonoBehaviour
+{
+
+    [SerializeField][Range(1,30)]private int height=10;
+    [SerializeField][Range(1,30)]private int width=10;
+
+    [SerializeField]private Transform wallPrefab = null;
+
+    private float size = 1.0f;                  
+
+//``````````````````````````````````````````````````````````````````````````````````````````````````````````
+//``````````````````````````````````````````````````````````````````````````````````````````````````````````
+    void Start()
+    {
+        //creation of maze matrix at start
+        var maze = MazeCreator.Create(height,width);
+        Draw(maze);
+    }
+
+    void Draw(StateOfWall[,] maze){
+        //actual drawing of maze using wall prefab the matrix data created
+        
+        for (int i = 0; i < width; ++i){
+            for (int j = 0; j < height; ++j){
+
+                var cell = maze[i, j];
+                var position = new Vector3(-(width / 2) + i, 0, (-height / 2) + j);
+
+                if (cell.HasFlag(StateOfWall.Up))
+                {
+                    var topWall = Instantiate(wallPrefab, transform) as Transform;
+                    topWall.position = position + new Vector3(0, 0, size / 2);
+                    topWall.localScale = new Vector3(size, topWall.localScale.y, topWall.localScale.z);
+                }
+
+                if (cell.HasFlag(StateOfWall.Left))
+                {
+                    var leftWall = Instantiate(wallPrefab, transform) as Transform;
+                    leftWall.position = position + new Vector3(-size / 2, 0, 0);
+                    leftWall.localScale = new Vector3(size, leftWall.localScale.y, leftWall.localScale.z);
+                    leftWall.eulerAngles = new Vector3(0, 90, 0);
+                }
+
+                if (i == width - 1)
+                {
+                    if (cell.HasFlag(StateOfWall.Right))
+                    {
+                        var rightWall = Instantiate(wallPrefab, transform) as Transform;
+                        rightWall.position = position + new Vector3(size / 2, 0, 0);
+                        rightWall.localScale = new Vector3(size, rightWall.localScale.y, rightWall.localScale.z);
+                        rightWall.eulerAngles = new Vector3(0, 90, 0);
+                    }
+                }
+
+                if (j == 0)
+                {
+                    if (cell.HasFlag(StateOfWall.Down))
+                    {
+                        var bottomWall = Instantiate(wallPrefab, transform) as Transform;
+                        bottomWall.position = position + new Vector3(0, 0, -size / 2);
+                        bottomWall.localScale = new Vector3(size, bottomWall.localScale.y, bottomWall.localScale.z);
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
